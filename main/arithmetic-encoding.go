1aa5dbad9faedc3a5a5b9293d97baa3ee42abe1b
Burmau Garba <burmaud1@gmail.com>
2024-06-13 16:37:58 -0400

    Modified mistakes of os.Args

--- main/arithmetic-encoding.go
+++ main/arithmetic-encoding.go
@@ -1,19 +1,18 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
-	fmt.Print("hello arithmetic encoding")
+	fmt.Println("hello arithmetic encoding")
 	// read read a file
-	file, err := os.Args[1:]
-	if err != nil {
-		log.Fatal(err.New(err))
+	if len(os.Args[1:]) <= 0 {
+		log.Fatal(errors.New(" Add files for compression in the command line"))
 	}
-
 	// for each file i want to have a freq table for the entries
 	for _, file := range os.Args[1:] {
 		fmt.Printf("file %f\n", file)
